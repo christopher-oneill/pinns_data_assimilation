beccf93b0a77146b15624b0d0963990eafb838fe
christopher-oneill <christopher.mark.oneill@gmail.com>
2023-05-11 03:19:11 -0700

    added mfg job with distribute

---
+++ slurm/sjob_mfg_mean001_1h.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --account=def-martinuz    
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=3
+#SBATCH --mem=16000M
+#SBATCH --time=0-23:00
+#SBATCH --mail-user=christopher.mark.oneill@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/coneill/sync/logs/slurm-%A.out
+#SBATCH --array=1-3%1   # Run a 10-job array, one job at a time.
+# ---------------------------------------------------------------------
+echo "Current working directory: `pwd`"
+echo "Starting run at: `date`"
+# ---------------------------------------------------------------------
+echo ""
+echo "Job Array ID / Job ID: $SLURM_ARRAY_JOB_ID / $SLURM_JOB_ID"
+echo "This is job $SLURM_ARRAY_TASK_ID out of $SLURM_ARRAY_TASK_COUNT jobs."
+echo ""
+# ---------------------------------------------------------------------
+# Run your simulation step here...
+CASENAME="CC0018"
+
+cd $SLURM_TMPDIR
+
+mkdir $SLURM_TMPDIR/code/
+cp /home/coneill/sync/code/pinns_galerkin_viv.tar $SLURM_TMPDIR/code/
+tar -xf $SLURM_TMPDIR/code/pinns_galerkin_viv.tar -C $SLURM_TMPDIR/code/
+
+module load python/3.10
+source /home/coneill/sync/venv/tf1/bin/activate
+
+python $SLURM_TMPDIR/code/pinns_galerkin_viv/training_scripts/kevin_LD2TD2/LD2TD2_"$CASENAME"_23h.py
+
+
+# ---------------------------------------------------------------------
+echo "Job finished with exit code $? at: `date`"
+# ---------------------------------------------------------------------
+
+
---
+++ training_scripts/kevin_LD2TD2/plot_LD2TD2_modes6.py
@@ -0,0 +1,353 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plot
+import scipy as sp
+
+base_dir = 'C:/projects/pinns_local/data/mazi_fixed/'
+meanFieldFile = h5py.File(base_dir+'meanField.mat','r')
+configFile = h5py.File(base_dir+'configuration.mat','r')
+meanPressureFile = h5py.File(base_dir+'meanPressure.mat','r')
+reynoldsStressFile = h5py.File(base_dir+'reynoldsStresses.mat','r')
+meanGradientsFile = h5py.File(base_dir+'meanGradients.mat','r')
+
+print_name ='20230328_reynolds_stress_ep250'
+predfilename = base_dir+'20230328_reynolds_stress/dense50x10_b32_ep250_st2_pred.mat'
+predFile =  h5py.File(predfilename,'r')
+SaveFig = True
+PlotFig = False
+
+ux = np.array(meanFieldFile['meanField'][0,:]).transpose()
+ux = ux[:,0]
+uy = np.array(meanFieldFile['meanField'][1,:]).transpose()
+uy = uy[:,0]
+p = np.array(meanPressureFile['meanPressure']).transpose()
+p = p[:,0]
+upup = np.array(reynoldsStressFile['reynoldsStresses'][0,:]).transpose()
+upvp = np.array(reynoldsStressFile['reynoldsStresses'][1,:]).transpose()
+vpvp = np.array(reynoldsStressFile['reynoldsStresses'][2,:]).transpose()
+dudy = (np.array(meanGradientsFile['meanGradients'][1,:]).transpose())
+dvdx = np.array(meanGradientsFile['meanGradients'][2,:]).transpose()
+x = np.array(configFile['X'][0,:])
+y = np.array(configFile['X'][1,:])
+d = np.array(configFile['cylinderDiameter'])[0]
+
+MAX_upup = np.max(upup)
+MAX_upvp = np.max(upvp) # estimated maximum of nut # THIS VALUE is internally multiplied with 0.001 (mm and m)
+MAX_vpvp = np.max(vpvp)
+MAX_p= 1 # estimated maximum pressure
+
+nu_mol = 0.0066667
+ux_pred = np.array(predFile['pred'][:,0])*np.max(ux)
+uy_pred = np.array(predFile['pred'][:,1])*np.max(uy)
+p_pred = np.array(predFile['pred'][:,5])*MAX_p
+#nu_pred =  np.power(np.array(predFile['pred'][:,3]),2)*MAX_nut
+upup_pred = np.array(predFile['pred'][:,2])*MAX_upup
+upvp_pred = np.array(predFile['pred'][:,3])*MAX_upvp
+vpvp_pred = np.array(predFile['pred'][:,4])*MAX_vpvp
+# compute the estimated reynolds stress
+
+#upvp_pred = -np.multiply(np.reshape(nu_pred+nu_mol,[nu_pred.shape[0],1]),dudy+dvdx)
+
+print('ux.shape: ',ux.shape)
+print('uy.shape: ',uy.shape)
+print('p.shape: ',p.shape)
+print('upvp.shape: ',upvp.shape)
+print('dudy.shape: ',dudy.shape)
+print('dvdx.shape: ',dvdx.shape)
+
+print('x.shape: ',x.shape)
+print('y.shape: ',y.shape)
+print('d: ',d.shape)
+
+print('ux_pred.shape: ',ux_pred.shape)
+print('uy_pred.shape: ',uy_pred.shape)
+print('p_pred.shape: ',p_pred.shape)
+#print('nu_pred.shape: ',nu_pred.shape)
+print('upvp_pred.shape: ',upvp_pred.shape)
+
+# note that the absolute value of the pressure doesnt matter, only grad p and grad2 p, so subtract the mean 
+#p_pred = p_pred-(1/3)*(upup+vpvp)#p_pred - (1/3)*(upup+vpvp)
+
+
+
+x_vec = np.arange(-2,10,0.01)
+y_vec = np.arange(-2,2,0.01)
+[x_grid,y_grid] = np.meshgrid(x_vec,y_vec)
+ux_grid = sp.interpolate.griddata((x,y),ux,(x_grid,y_grid),method='cubic')
+uy_grid = sp.interpolate.griddata((x,y),uy,(x_grid,y_grid),method='cubic')
+p_grid = sp.interpolate.griddata((x,y),p,(x_grid,y_grid),method='cubic')
+upup_grid = sp.interpolate.griddata((x,y),upup,(x_grid,y_grid),method='cubic')
+upvp_grid = sp.interpolate.griddata((x,y),upvp,(x_grid,y_grid),method='cubic')
+vpvp_grid = sp.interpolate.griddata((x,y),vpvp,(x_grid,y_grid),method='cubic')
+ux_pred_grid = sp.interpolate.griddata((x,y),ux_pred,(x_grid,y_grid),method='cubic')
+uy_pred_grid = sp.interpolate.griddata((x,y),uy_pred,(x_grid,y_grid),method='cubic')
+p_pred_grid = sp.interpolate.griddata((x,y),p_pred,(x_grid,y_grid),method='cubic')
+#nu_pred_grid = sp.interpolate.griddata((x,y),nu_pred,(x_grid,y_grid),method='cubic')
+upup_pred_grid =  sp.interpolate.griddata((x,y),upup_pred,(x_grid,y_grid),method='cubic')
+upvp_pred_grid =  sp.interpolate.griddata((x,y),upvp_pred,(x_grid,y_grid),method='cubic')
+vpvp_pred_grid =  sp.interpolate.griddata((x,y),vpvp_pred,(x_grid,y_grid),method='cubic')
+ux_diff_grid = sp.interpolate.griddata((x,y),ux-ux_pred,(x_grid,y_grid),method='cubic')
+uy_diff_grid = sp.interpolate.griddata((x,y),uy-uy_pred,(x_grid,y_grid),method='cubic')
+p_diff_grid = sp.interpolate.griddata((x,y),p-p_pred,(x_grid,y_grid),method='cubic')
+upup_diff_grid = sp.interpolate.griddata((x,y),upup-upup_pred,(x_grid,y_grid),method='cubic')
+upvp_diff_grid = sp.interpolate.griddata((x,y),upvp-upvp_pred,(x_grid,y_grid),method='cubic')
+vpvp_diff_grid = sp.interpolate.griddata((x,y),vpvp-vpvp_pred,(x_grid,y_grid),method='cubic')
+
+
+ux_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+uy_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+p_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+ux_pred_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+uy_pred_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+p_pred_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+#nu_pred_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+upup_pred_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+upvp_pred_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+vpvp_pred_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+ux_diff_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+uy_diff_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+p_diff_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+upup_diff_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+upvp_diff_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+vpvp_diff_grid[np.power(np.power(x_grid,2)+np.power(y_grid,2),0.5)<=0.5*d]=np.NaN
+
+f1_max = np.nanmax(np.array([np.nanmax(ux_grid)]))
+f1_min = np.nanmin(np.array([np.nanmin(ux_grid)]))
+f1_lims = np.nanmax(np.abs(np.array([f1_max,f1_min])))
+f1_levels = np.linspace(-f1_lims,f1_lims,21)
+fig = plot.figure(1)
+ax = fig.add_subplot(3,1,1)
+plot.axis('equal')
+plot.contourf(x_grid,y_grid,ux_grid,levels=f1_levels)
+plot.set_cmap('bwr')
+plot.colorbar()
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.ylabel('y/D')
+fig.add_subplot(3,1,2)
+plot.contourf(x_grid,y_grid,ux_pred_grid,levels=f1_levels)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.ylabel('y/D')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.axis('equal')
+fig.add_subplot(3,1,3)
+plot.contourf(x_grid,y_grid,ux_diff_grid,levels=f1_levels)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.ylabel('y/D')
+plot.xlabel('x/D')
+plot.axis('equal')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+if SaveFig:
+    plot.savefig(base_dir+'figures/'+print_name+'_mean_ux.png',dpi=300)
+
+
+
+f2_max = np.nanmax(np.array([np.nanmax(uy_grid)]))
+f2_min = np.nanmin(np.array([np.nanmin(uy_grid)]))
+f2_lims = np.nanmax(np.abs(np.array([f2_max,f2_min])))
+f2_levels = np.linspace(-f2_lims,f2_lims,21)
+fig2 = plot.figure(2)
+fig2.add_subplot(3,1,1)
+plot.axis('equal')
+plot.contourf(x_grid,y_grid,uy_grid,levels=f2_levels)
+plot.set_cmap('bwr')
+plot.colorbar()
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.ylabel('y/D')
+fig2.add_subplot(3,1,2)
+plot.contourf(x_grid,y_grid,uy_pred_grid,levels=f2_levels)
+plot.set_cmap('bwr')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.colorbar()
+plot.axis('equal')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.ylabel('y/D')
+fig2.add_subplot(3,1,3)
+plot.contourf(x_grid,y_grid,uy_diff_grid,levels=f2_levels)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.ylabel('y/D')
+plot.xlabel('x/D')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.axis('equal')
+if SaveFig:
+    plot.savefig(base_dir+'figures/'+print_name+'_mean_uy.png',dpi=300)
+
+f3_max = np.nanmax(np.array([np.nanmax(p_grid)]))
+f3_min = np.nanmin(np.array([np.nanmin(p_grid)]))
+f3_lims = np.nanmax(np.abs(np.array([f3_max,f3_min])))
+f3_levels = np.linspace(-f3_lims,f3_lims,21)
+fig3 = plot.figure(3)
+fig3.add_subplot(3,1,1)
+plot.axis('equal')
+plot.contourf(x_grid,y_grid,p_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.ylabel('y/D')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+fig3.add_subplot(3,1,2)
+plot.contourf(x_grid,y_grid,p_pred_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.axis('equal')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.ylabel('y/D')
+fig3.add_subplot(3,1,3)
+plot.contourf(x_grid,y_grid,p_diff_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.axis('equal')
+plot.ylabel('y/D')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.xlabel('x/D')
+if SaveFig:
+    plot.savefig(base_dir+'figures/'+print_name+'_mean_p.png',dpi=300)
+
+f4_max = np.nanmax(np.array([np.nanmax(upup_grid)]))
+f4_min = np.nanmin(np.array([np.nanmin(upup_grid)]))
+f4_lims = np.nanmax(np.abs(np.array([f4_max,f4_min])))
+f4_levels = np.linspace(-f4_lims,f4_lims,21)
+fig4 = plot.figure(4)
+fig4.add_subplot(3,1,1)
+plot.axis('equal')
+plot.contourf(x_grid,y_grid,upup_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.ylabel('y/D')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+fig4.add_subplot(3,1,2)
+plot.contourf(x_grid,y_grid,upup_pred_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.axis('equal')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.ylabel('y/D')
+fig4.add_subplot(3,1,3)
+plot.contourf(x_grid,y_grid,upup_diff_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.axis('equal')
+plot.ylabel('y/D')
+plot.xlabel('x/D')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+if SaveFig:
+    plot.savefig(base_dir+'figures/'+print_name+'_mean_upup.png',dpi=300)
+
+f5_max = np.nanmax(np.array([np.nanmax(upvp_grid)]))
+f5_min = np.nanmin(np.array([np.nanmin(upvp_grid)]))
+f5_lims = np.nanmax(np.abs(np.array([f5_max,f5_min])))
+f5_levels = np.linspace(-f5_lims,f5_lims,21)
+fig5 = plot.figure(5)
+fig5.add_subplot(3,1,1)
+plot.axis('equal')
+plot.contourf(x_grid,y_grid,upvp_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.ylabel('y/D')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+fig5.add_subplot(3,1,2)
+plot.contourf(x_grid,y_grid,upvp_pred_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.axis('equal')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.ylabel('y/D')
+fig5.add_subplot(3,1,3)
+plot.contourf(x_grid,y_grid,upvp_diff_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.axis('equal')
+plot.ylabel('y/D')
+plot.xlabel('x/D')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+if SaveFig:
+    plot.savefig(base_dir+'figures/'+print_name+'_mean_upvp.png',dpi=300)
+
+f6_max = np.nanmax(np.array([np.nanmax(vpvp_grid)]))
+f6_min = np.nanmin(np.array([np.nanmin(vpvp_grid)]))
+f6_lims = np.nanmax(np.abs(np.array([f6_max,f6_min])))
+f6_levels = np.linspace(-f6_lims,f6_lims,21)
+fig6 = plot.figure(6)
+fig6.add_subplot(3,1,1)
+plot.axis('equal')
+plot.contourf(x_grid,y_grid,vpvp_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.ylabel('y/D')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+fig6.add_subplot(3,1,2)
+plot.contourf(x_grid,y_grid,vpvp_pred_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.axis('equal')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+plot.ylabel('y/D')
+fig6.add_subplot(3,1,3)
+plot.contourf(x_grid,y_grid,vpvp_diff_grid,21)
+plot.set_cmap('bwr')
+plot.colorbar()
+plot.axis('equal')
+plot.ylabel('y/D')
+plot.xlabel('x/D')
+ax=plot.gca()
+ax.set_xlim(left=-2.0,right=10.0)
+ax.set_ylim(bottom=-2.0,top=2.0)
+if SaveFig:
+    plot.savefig(base_dir+'figures/'+print_name+'_mean_vpvp.png',dpi=300)
+
+if False:
+    f5_max = np.nanmax(np.array([np.nanmax(nu_pred_grid)]))
+    f5_min = np.nanmin(np.array([np.nanmin(nu_pred_grid)]))
+    f5_lims = np.nanmax(np.abs(np.array([f5_max,f5_min])))
+    f5_levels = np.linspace(-f5_lims,f5_lims,21)
+    fig5 = plot.figure(5)
+    fig5.add_subplot(3,1,1)
+    plot.axis('equal')
+    plot.contourf(x_grid,y_grid,nu_pred_grid,levels=f5_levels)
+    plot.set_cmap('bwr')
+    plot.colorbar()
+    plot.ylabel('y/D')
+    plot.xlabel('x/D')
+    ax=plot.gca()
+    ax.set_xlim(left=-2.0,right=10.0)
+    ax.set_ylim(bottom=-2.0,top=2.0)
+    plot.savefig(base_dir+'figures/'+print_name+'_mean_nu.png',dpi=300)
+if PlotFig:
+    plot.show()
