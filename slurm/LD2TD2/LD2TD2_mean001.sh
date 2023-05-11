7ae70e335bd0f2ea295ef86187b72617893dceeb
christopher-oneill <christopher.mark.oneill@gmail.com>
2023-05-11 07:23:18 -0700

    fixed LD2

--- slurm/LD2TD2/LD2TD2_mean001.sh
+++ slurm/LD2TD2/LD2TD2_mean001.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-martinuz    
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=3
-#SBATCH --mem=32000M
+#SBATCH --mem=16000M
 #SBATCH --time=0-23:00
 #SBATCH --mail-user=christopher.mark.oneill@gmail.com
 #SBATCH --mail-type=ALL
@@ -17,5 +17,5 @@ tar -xf $SLURM_TMPDIR/code/pinns_galerkin_viv.tar -C $SLURM_TMPDIR/code/
 module load python/3.10
 source /home/coneill/sync/venv/tf1/bin/activate
 
-python $SLURM_TMPDIR/code/pinns_galerkin_viv/training_scripts/mazi_fixed_grid/LD2TD2_mean001.py
+python $SLURM_TMPDIR/code/pinns_galerkin_viv/training_scripts/LD2TD2/LD2TD2_mean001.py
 
