f3a28c795aab796fae4cb10f0f83938b70426e49
christopher-oneill <christopher.mark.oneill@gmail.com>
2023-05-25 07:52:15 -0700

    added new jobs for LD2wm

---
+++ slurm/LD2TD2_wake/LD2wm009.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=def-martinuz    
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=32000M
+#SBATCH --time=0-23:00
+#SBATCH --mail-user=christopher.mark.oneill@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/coneill/sync/logs/slurm-LD2wm009-%A.out
+
+cd $SLURM_TMPDIR
+
+module load python/3.10
+source /home/coneill/sync/venv/tf1/bin/activate
+
+python  /home/coneill/sync/code/pinns_galerkin_viv/training_scripts/LD2TD2_wake/LD2TD2_wake_mean009.py
+
