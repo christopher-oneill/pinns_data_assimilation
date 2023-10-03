e36a3f43b8a96e749a1a2c4e37d62a622db26a9b
christopher-oneill <christopher.mark.oneill@gmail.com>
2023-10-03 13:37:44 -0600

    fixed bug in dft submission scripts

--- slurm/mazi_fixed_grid/dft/mfg_dft2_S1_j002.sh
+++ slurm/mazi_fixed_grid/dft/mfg_dft2_S1_j002.sh
@@ -18,7 +18,7 @@ cd $SLURM_TMPDIR
 module load python/3.10
 source /home/coneill/sync/venv/tf1/bin/activate
 
-python /home/coneill/sync/code/pinns_galerkin_viv/training_scripts/mazi_fixed_grid/mfg_new_fourier.py 2 1 2
+python /home/coneill/sync/code/pinns_galerkin_viv/training_scripts/mazi_fixed_grid/mfg_new_dft.py 2 1 2
 
 
 # ---------------------------------------------------------------------
