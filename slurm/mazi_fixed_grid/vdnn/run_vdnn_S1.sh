c3ec7b427485abb6b06912066713b05d67aac11c
christopher-oneill <christopher.mark.oneill@gmail.com>
2023-11-27 18:45:24 -0700

    fixed run script

--- slurm/mazi_fixed_grid/vdnn/run_vdnn_S1.sh
+++ slurm/mazi_fixed_grid/vdnn/run_vdnn_S1.sh
@@ -3,30 +3,7 @@
 cd /home/coneill/sync/
 
 
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L4N75.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L4N75.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L4N100.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L4N100.sh
-
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L6N50.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L6N50.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L6N75.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L6N75.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L6N100.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L6N100.sh
-
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L8N50.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L8N50.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L8N75.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L8N75.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L8N100.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L8N100.sh
-
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L10N25.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L10N25.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L10N50.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L10N50.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L10N75.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L10N75.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m001_L10N100.sh
-sbatch code/pinns_galerkin_viv/slurm/mazi_fixed_grid/vdnn/S1/mfg_vdnn_S1_m002_L10N100.sh
+sbatch code/pinns_data_assimilation/slurm/mazi_fixed_grid/vdnn/S1/m010.sh
+sbatch code/pinns_data_assimilation/slurm/mazi_fixed_grid/vdnn/S1/m011.sh
+sbatch code/pinns_data_assimilation/slurm/mazi_fixed_grid/vdnn/S1/m012.sh
+sbatch code/pinns_data_assimilation/slurm/mazi_fixed_grid/vdnn/S1/m013.sh
