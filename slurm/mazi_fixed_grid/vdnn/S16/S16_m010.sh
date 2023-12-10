45bd509230c6209ffdc8471b46553c8d05a8da00
christopher-oneill <christopher.mark.oneill@gmail.com>
2023-12-10 16:11:48 -0700

    reinsteated jobs vdnn10-13

---
+++ slurm/mazi_fixed_grid/vdnn/S16/S16_m010.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --account=def-martinuz    
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=123G
+#SBATCH --time=0-23:30
+#SBATCH --mail-user=christopher.mark.oneill@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/coneill/sync/logs/vdnn/S16/m010-%A.out
+
+# ---------------------------------------------------------------------
+echo "Current working directory: `pwd`"
+echo "Starting run at: `date`"
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Run your simulation step here...
+cd $SLURM_TMPDIR
+
+module load python/3.10
+source /home/coneill/sync/venv/tf1/bin/activate
+
+python /home/coneill/sync/code/pinns_data_assimilation/training_scripts/mazi_fixed_grid/mfg_vdnn_mean.py 10 16 10 100 23
+
+
+# ---------------------------------------------------------------------
+echo "Job finished with exit code $? at: `date`"
+# ---------------------------------------------------------------------
+
+sbatch /home/coneill/sync/code/slurm/mazi_fixed_grid/vdnn/S16/S16_m010.sh
