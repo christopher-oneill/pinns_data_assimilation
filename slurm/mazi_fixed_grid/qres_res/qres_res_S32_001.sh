67e65e69e62a3e6c66dc75f2273235014f170f7a
christopher-oneill <christopher.mark.oneill@gmail.com>
2024-01-24 12:58:17 -0700

    updated gpu jobs

---
+++ slurm/mazi_fixed_grid/qres_res/qres_res_S32_001.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --account=def-martinuz
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=120G
+#SBATCH --time=0-11:30
+#SBATCH --mail-user=christopher.mark.oneill@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/coneill/sync/logs/mfg_qres_res_S32_m001-%A.out
+
+# ---------------------------------------------------------------------
+echo "Current working directory: `pwd`"
+echo "Starting run at: `date`"
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Run your simulation step here...
+cd $SLURM_TMPDIR
+
+module load python/3.10
+source /home/coneill/sync/venv/tf1/bin/activate
+
+python /home/coneill/sync/code/pinns_data_assimilation/training_scripts/mazi_fixed_grid/mfg_qres_res.py 1 32 11
+
+
+# ---------------------------------------------------------------------
+echo "Job finished with exit code $? at: `date`"
+# ---------------------------------------------------------------------
+
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/qres_res/qres_res_S32_001.sh
--- slurm/mazi_fixed_grid/qres_res/qres_res_S4_001.sh
+++ slurm/mazi_fixed_grid/qres_res/qres_res_S4_001.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=0-11:30
 #SBATCH --mail-user=christopher.mark.oneill@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH --output=/home/coneill/sync/logs/mfg_qres_res_S1_m001-%A.out
+#SBATCH --output=/home/coneill/sync/logs/mfg_qres_res_S4_m001-%A.out
 
 # ---------------------------------------------------------------------
 echo "Current working directory: `pwd`"
@@ -20,11 +20,11 @@ cd $SLURM_TMPDIR
 module load python/3.10
 source /home/coneill/sync/venv/tf1/bin/activate
 
-python /home/coneill/sync/code/pinns_data_assimilation/training_scripts/mazi_fixed_grid/mfg_qres_res.py 1 32 11
+python /home/coneill/sync/code/pinns_data_assimilation/training_scripts/mazi_fixed_grid/mfg_qres_res.py 1 4 11
 
 
 # ---------------------------------------------------------------------
 echo "Job finished with exit code $? at: `date`"
 # ---------------------------------------------------------------------
 
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/dense/d_lg_S1_m001.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/qres_res/qres_res_S4_001.sh
