1f22a328986a817c081584b356a078b890d32567
christopher-oneill <christopher.mark.oneill@gmail.com>
2024-03-11 21:04:15 -0600

    added fouirier grid jobs

---
+++ slurm/mazi_fixed_grid/fbc_f/fbc_f002_j1_f1_S16.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --account=def-martinuz
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=120G
+#SBATCH --time=0-23:30
+#SBATCH --mail-user=christopher.mark.oneill@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/coneill/sync/logs/fbcf002_j1_f1_S16-%A.out
+
+# ---------------------------------------------------------------------
+echo "Current working directory: `pwd`"
+echo "Starting run at: `date`"
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Run your simulation step here...
+
+module load python/3.10
+source /home/coneill/sync/venv/tf1/bin/activate
+
+python /home/coneill/sync/code/pinns_data_assimilation/training_scripts/mazi_fixed_grid/mfg_fbc_f002.py 1 1 16 23
+
+
+# ---------------------------------------------------------------------
+echo "Job finished with exit code $? at: `date`"
+# ---------------------------------------------------------------------
+
+
