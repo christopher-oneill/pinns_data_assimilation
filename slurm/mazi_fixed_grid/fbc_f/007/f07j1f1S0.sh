ce878b23a834e4c71032978537a1525727954746
christopher-oneill <christopher.mark.oneill@gmail.com>
2024-03-27 12:40:44 -0600

    renamed slurm

---
+++ slurm/mazi_fixed_grid/fbc_f/007/f07j1f1S0.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --account=def-martinuz
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=31G
+#SBATCH --gres=gpu:a5000:1
+#SBATCH --time=0-23:30
+#SBATCH --mail-user=christopher.mark.oneill@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/coneill/sync/logs/f07j1f1S0-%A.out
+
+# ---------------------------------------------------------------------
+echo "Current working directory: `pwd`"
+echo "Starting run at: `date`"
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Run your simulation step here...
+
+module load python/3.10
+source /home/coneill/sync/venv/tf1/bin/activate
+
+python /home/coneill/sync/code/pinns_data_assimilation/training_scripts/mazi_fixed_grid/mfg_fbc_f007.py 1 1 0 23
+
+
+# ---------------------------------------------------------------------
+echo "Job finished with exit code $? at: `date`"
+# ---------------------------------------------------------------------
+
+
--- slurm/mazi_fixed_grid/fbc_f/007/f07j1f2S0.sh
+++ slurm/mazi_fixed_grid/fbc_f/007/f07j1f2S0.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=0-23:30
 #SBATCH --mail-user=christopher.mark.oneill@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH --output=/home/coneill/sync/logs/f07_j1_f0_S0-%A.out
+#SBATCH --output=/home/coneill/sync/logs/f07j1f2S0-%A.out
 
 # ---------------------------------------------------------------------
 echo "Current working directory: `pwd`"
@@ -19,7 +19,7 @@ echo "Starting run at: `date`"
 module load python/3.10
 source /home/coneill/sync/venv/tf1/bin/activate
 
-python /home/coneill/sync/code/pinns_data_assimilation/training_scripts/mazi_fixed_grid/mfg_fbc_f007.py 1 0 0 23
+python /home/coneill/sync/code/pinns_data_assimilation/training_scripts/mazi_fixed_grid/mfg_fbc_f007.py 1 2 0 23
 
 
 # ---------------------------------------------------------------------
