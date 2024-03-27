d51675ae19d44e9ddff434d6ebbb78df4a64101d
christopher-oneill <christopher.mark.oneill@gmail.com>
2024-03-27 14:51:54 -0600

    updated jobs

--- slurm/mazi_fixed_grid/fbc_f/007/f07j1f1S0.sh
+++ slurm/mazi_fixed_grid/fbc_f/007/f07j1f1S0.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-martinuz
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=31G
-#SBATCH --gres=gpu:a5000:1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=120G
 #SBATCH --time=0-23:30
 #SBATCH --mail-user=christopher.mark.oneill@gmail.com
 #SBATCH --mail-type=ALL
