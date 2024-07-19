9a258ce44a5fa45232d854decc2c71b141b01a83
christopher-oneill <christopher.mark.oneill@gmail.com>
2024-07-18 21:42:01 -0600

    fixed submission script

--- slurm/mazi_fixed_grid/test/f3/f_jobs.sh
+++ slurm/mazi_fixed_grid/test/f3/f_jobs.sh
@@ -1,43 +1,43 @@
 
 
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f0_j1_S0.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f1_j1_S0.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f2_j1_S0.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f3_j1_S0.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f4_j1_S0.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f5_j1_S0.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f0_j1_S0.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f1_j1_S0.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f2_j1_S0.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f3_j1_S0.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f4_j1_S0.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f5_j1_S0.sh
 
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f0_j1_S2.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f1_j1_S2.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f2_j1_S2.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f3_j1_S2.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f4_j1_S2.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f5_j1_S2.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f0_j1_S2.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f1_j1_S2.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f2_j1_S2.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f3_j1_S2.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f4_j1_S2.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f5_j1_S2.sh
 
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f0_j1_S4.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f1_j1_S4.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f2_j1_S4.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f3_j1_S4.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f4_j1_S4.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f5_j1_S4.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f0_j1_S4.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f1_j1_S4.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f2_j1_S4.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f3_j1_S4.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f4_j1_S4.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f5_j1_S4.sh
 
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f0_j1_S8.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f1_j1_S8.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f2_j1_S8.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f3_j1_S8.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f4_j1_S8.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f5_j1_S8.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f0_j1_S8.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f1_j1_S8.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f2_j1_S8.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f3_j1_S8.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f4_j1_S8.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f5_j1_S8.sh
 
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f0_j1_S16.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f1_j1_S16.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f2_j1_S16.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f3_j1_S16.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f4_j1_S16.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f5_j1_S16.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f0_j1_S16.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f1_j1_S16.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f2_j1_S16.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f3_j1_S16.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f4_j1_S16.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f5_j1_S16.sh
 
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f0_j1_S32.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f1_j1_S32.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f2_j1_S32.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f3_j1_S32.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f4_j1_S32.sh
-sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_f1f3_f5_j1_S32.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f0_j1_S32.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f1_j1_S32.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f2_j1_S32.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f3_j1_S32.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f4_j1_S32.sh
+sbatch /home/coneill/sync/code/pinns_data_assimilation/slurm/mazi_fixed_grid/test/f3/mfg_t1f3_f5_j1_S32.sh
