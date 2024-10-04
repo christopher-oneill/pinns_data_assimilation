87710257b99fe96f02399e4c60a4245b09b87439
christopher-oneill <christopher.mark.oneill@gmail.com>
2024-10-04 16:19:56 -0600

    added model saving

--- training_scripts/mazi_fixed_grid/test/mfg_t010_f001.py
+++ training_scripts/mazi_fixed_grid/test/mfg_t010_f001.py
@@ -1248,6 +1248,8 @@ if __name__=="__main__":
     if model_file is not None:
         with tf.device(tf_device_string):
             model_FANS,training_steps = load_custom()
+            model_FANS.save(savedir+job_name+'_ep'+str(training_steps)+'_model.h5')
+            exit()
     else:
         # create a new model
         training_steps = 0
